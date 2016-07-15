28ad200b61bad34a5625b53ff73d4b496efc85aa
Pratik Agravat <agravat202@gmail.com>
2016-07-15 19:42:52 +0530

    15-7-2016
    nothing change :)

--- static/index.php
+++ static/index.php
@@ -1,58 +1,72 @@
-<?php 
+<?php
+
 global $a;
 
 interface log
 {
-    
-    function  check();
-   
+
+    public function check();
 }
 
-class main implements log {
-    
-    function main()
+class main implements log
+{
+
+    public function __construct()
     {
         echo $a = 5;
-        echo "<br> this is globle variable of sub class <br>";// globle 
+        echo "<br> this is globle variable of sub class <br>"; // globle 
         echo "<br>this is constoctor of main <br> ";
         echo $this->check();
         echo $this->p();
     }
+
     public function check()
     {
         echo "<br>this is check function of main impliment by log <br> ";
     }
-    function p()
+
+    public function p()
     {
-        echo "<br>this is p class from main by polymophisam<br>";
+        echo "<br>this is p method from main by polymophisam<br>";
     }
+
 }
-abstract class who extends main implements log{
+
+abstract class who extends main implements log
+{
+
     abstract function ch();
+
     public function check()
     {
         echo "<br>this is check function of abstrac class impliment by log <br> ";
     }
+
 }
+
 class sub extends who
 {
-    function sub()
+
+    public function __construct()
     {
         echo $a = 10;
-        
+
         echo "<br> this is globle variable of sub class <br>";
-        echo $this->ch()."<br>";
-         echo $this->p()."<br>";
+        echo $this->ch() . "<br>";
+        echo $this->p() . "<br>";
     }
-            function ch()
+
+    public function ch()
     {
         echo "<br>this is ch from sub class<br>";
     }
-    
-    funciton p()
+
+    public function p()
     {
         echo "<br>this is p class from sub by polymophisam  <br>";
     }
+
 }
+
 $a = new sub();
 $b = new main();
